0ca15f8e5482715cbaf1f5a0b93fc0d38dc0ecb8
ffasdd <nhc0830@naver.com>
2022-11-01 05:13:33 +0900

    rhino move
    코뿔소 움직임 구현

--- stage_1.py
+++ stage_1.py
@@ -1,3 +1,5 @@
+import random
+import time
 import pico2d
 from pico2d import *
 import game_framework
@@ -5,12 +7,24 @@ import stage_select_state
 
 running = True
 
+PIXEL_PER_METER = (10.0 / 0.4) # 10 pixel 30 cm
+RUN_SPEED_KMPH = 30.0 # Km / Hour
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
 class BG:
     def __init__(self):
         self.image = load_image('resource/background1.png')
 
     def draw(self):
-        self.image.draw(530, 510)
+        global x
+        self.image.draw(1060-x*2, 510)
+
 class UI:
     def __init__(self):
         self.image = load_image('resource/ui.png')
@@ -19,20 +33,23 @@ class UI:
         self.image.draw(530, 160)
 class Player:
     def __init__(self):
-        self.x, self.y = 0,450
+        self.x, self.y = 0,400
         self.direction=1
         self.frame = 0
         self.image = load_image('resource/player/player_move.png')
 
     def update(self):
-        self.frame = (self.frame + 1) % 8
+        global x
+        self.frame =  (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
         if move == True:
             if dir == -1:
                 self.direction = -1
-                self.x += self.direction*8
+                self.x += self.direction * RUN_SPEED_PPS * game_framework.frame_time
+                x =self.x
             elif dir == 1:
                 self.direction = 1
-                self.x += self.direction*8
+                self.x += self.direction * RUN_SPEED_PPS * game_framework.frame_time
+                x=self.x
         if self.x > 800:
             self.x=800
         elif self.x<0:
@@ -41,33 +58,75 @@ class Player:
     def draw(self):
         if move == True:
             if dir == 1:
-                self.image.clip_draw(self.frame * 128, 0, 128, 120, self.x, self.y,256,240)
+                self.image.clip_draw(int(self.frame)* 210, 0, 205, 120, self.x, self.y,307.5,180)
             elif dir == -1:
-                self.image.clip_draw(self.frame * 128, 120, 128, 120, self.x, self.y,256,240)
+                self.image.clip_draw(int(self.frame)* 210, 120, 205, 120, self.x, self.y,307.5,180)
         elif move == False:
             if dir == 1:
-                self.image.clip_draw(3, 0, 128, 120, self.x, self.y,256,240)
+                self.image.clip_draw(3, 0, 210, 120, self.x, self.y,307.5,180)
             elif dir == -1:
-                self.image.clip_draw(3, 120, 128, 120, self.x, self.y,256,240)
+                self.image.clip_draw(3, 120, 210, 120, self.x, self.y,307.5,180)
+
 
 class Mouse:
     def __init__(self):
-        self.x, self.y = 0,400
+        global x
+        self.x, self.y = -x,400
         self.state = 0 # 0=walk/1=attack/2=die
         self.frame = 0
         self.image = load_image('resource/player/mouse.png')
 
     def update(self):
-        delay(0.05)
-        self.frame = (self.frame + 1) % 6
-        self.x += 5
-        if self.x > 800:
-            self.x = 800
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 6
+        self.x += 1 * RUN_SPEED_PPS * game_framework.frame_time
+        if self.x > 1060:
+            self.x = 1060
     def draw(self):
         if self.state == 0:
-            for x in range (0,mouse_num,1):
-                self.image.clip_draw(self.frame * 57, 0, 57,51, self.x, self.y,100,120)
+            self.image.clip_draw(int(self.frame) * 57, 0, 57,51, self.x, self.y,100,120)
 
+class Dragon:
+    def __init__(self):
+        global x
+        self.x, self.y = -x,400
+        self.state = 0 #  0=walk/1=attack/2=die
+        self.frame = 0
+        self.image = load_image('resource/player/dragon.png')
+
+    def update(self):
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 5
+        self.x += 1 * RUN_SPEED_PPS * game_framework.frame_time
+        if self.x > 1060:
+            self.x = 1060
+    def draw(self):
+        if self.state == 0:
+            self.image.clip_draw(int(self.frame) * 170, 0, 170,170, self.x, self.y,200,200)
+
+class Rhino:
+    def __init__(self):
+        global x
+        self.x, self.y = -x,400
+        self.state = 0 #  0=walk/1=attack/2=die
+        self.frame = 0
+        self.image = load_image('resource/player/rhinoceros.png')
+
+    def update(self):
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
+        self.x += 1 * RUN_SPEED_PPS * game_framework.frame_time/1.8
+        if self.x > 1060:
+            self.x = 1060
+    def draw(self):
+        if self.state == 0:
+            self.image.clip_draw(int(self.frame) * 128, 0, 128,149, self.x, self.y,166.4,193.7)
+
+class Mace_1():
+    pass
+
+class Mace_2():
+    pass
+
+class Mace_3():
+    pass
 
 def handle_events():
     global running, dir, move,mouse_num
@@ -84,49 +143,98 @@ def handle_events():
                     move = True
                     dir = 1
                 case pico2d.SDLK_KP_1:
-                    mouse_num +=1
+                    add_mouse()
+                case pico2d.SDLK_KP_2:
+                    add_dragon()
+                case pico2d.SDLK_KP_3:
+                    add_rhinos()
+                case pico2d.SDLK_j:
+                    attack_1()
+                case pico2d.SDLK_j:
+                    attack_2()
+                case pico2d.SDLK_j:
+                    attack_3()
                 case pico2d.SDLK_ESCAPE:
+                    exit()
                     game_framework.change_state(stage_select_state)
         elif event.type == SDL_KEYUP:
             move =False
 
 player = None
 background =None
-mouse = None
-mouse_num = 0
+mouses = []
+dragons = []
+rhinos = []
+maces_1 =[]
+maces_2=[]
+maces_3=[]
 ui =None
 move = False
 running = True
 dir = 0
+x =0
 
 def draw_world():
     background.draw()
     ui.draw()
     player.draw()
-    mouse.draw()
+    for mouse in mouses:
+        mouse.draw()
+    for dragon in dragons:
+        dragon.draw()
+    for rhino in rhinos:
+        rhino.draw()
 
 def enter():
-    global player, running, background, ui, mouse
-    running = True
+    global player, running, background, ui
     ui= UI()
     player = Player()
-    background =BG()
-    mouse = Mouse()
-    pass
+    background = BG()
+    running = True
 
 def exit():
-    global player, background, mouse
-    del mouse
+    global player, background
+    for mouse in mouses:
+        del mouse
+    for dragon in dragons:
+        del dragon
+    for rhino in rhinos:
+        del rhino
     del background
     del player
     pass
 
 def update():
+    for mouse in mouses:
+        mouse.update()
+    for dragon in dragons:
+        dragon.update()
+    for rhino in rhinos:
+        rhino.update()
     player.update()
-    mouse.update()
-
 def draw():
     clear_canvas()
     draw_world()
     update_canvas()
 
+def pause():
+    pass
+
+def resume():
+    pass
+
+def add_mouse():
+    mouses.append(Mouse())
+def add_dragon():
+    dragons.append(Dragon())
+def add_rhinos():
+    rhinos.append(Rhino())
+
+def attack_1():
+    maces_1.append(Mace_1())
+
+def attack_2():
+    maces_2.append(Mace_2())
+
+def attack_3():
+    maces_3.append(Mace_3())
--- stage_3.py
+++ stage_3.py
@@ -1,16 +1,22 @@
+import random
+
 import pico2d
 from pico2d import *
 import game_framework
 import stage_select_state
 
+
 running = True
 
 class BG:
     def __init__(self):
+        global x
         self.image = load_image('resource/background3.png')
 
     def draw(self):
-        self.image.draw(530, 510)
+        global x
+        self.image.draw(1600-(x*2), 510)
+
 class UI:
     def __init__(self):
         self.image = load_image('resource/ui.png')
@@ -19,40 +25,44 @@ class UI:
         self.image.draw(530, 160)
 class Player:
     def __init__(self):
-        self.x, self.y = 0,450
+        self.x, self.y = 0,400
         self.direction=1
         self.frame = 0
         self.image = load_image('resource/player/player_move.png')
 
     def update(self):
+        global x
         self.frame = (self.frame + 1) % 8
         if move == True:
             if dir == -1:
                 self.direction = -1
-                self.x += self.direction*8
+                self.x += self.direction*5
+                x=self.x
             elif dir == 1:
                 self.direction = 1
-                self.x += self.direction*8
-        if self.x > 800:
-            self.x=800
+                self.x += self.direction*5
+                x=self.x
+        if self.x > 1000:
+            self.x=1000
         elif self.x<0:
             self.x = 0
 
     def draw(self):
         if move == True:
             if dir == 1:
-                self.image.clip_draw(self.frame * 128, 0, 128, 120, self.x, self.y,256,240)
+                self.image.clip_draw(self.frame * 210, 0, 205, 120, self.x, self.y,307.5,180)
             elif dir == -1:
-                self.image.clip_draw(self.frame * 128, 120, 128, 120, self.x, self.y,256,240)
+                self.image.clip_draw(self.frame * 210, 120, 205, 120, self.x, self.y,307.5,180)
         elif move == False:
             if dir == 1:
-                self.image.clip_draw(3, 0, 128, 120, self.x, self.y,256,240)
+                self.image.clip_draw(3, 0, 210, 120, self.x, self.y,307.5,180)
             elif dir == -1:
-                self.image.clip_draw(3, 120, 128, 120, self.x, self.y,256,240)
+                self.image.clip_draw(3, 120, 210, 120, self.x, self.y,307.5,180)
+
 
 class Mouse:
     def __init__(self):
-        self.x, self.y = 0,450
+        self.x, self.y = random.randint(0,20),400
         self.state = 0 # 0=walk/1=attack/2=die
         self.frame = 0
         self.image = load_image('resource/player/mouse.png')
@@ -84,7 +94,7 @@ def handle_events():
                     move = True
                     dir = 1
                 case pico2d.SDLK_KP_1:
-                    mouse_num +=1
+                    game_framework.push_state(mouse_adjust)
                 case pico2d.SDLK_ESCAPE:
                     game_framework.change_state(stage_select_state)
         elif event.type == SDL_KEYUP:
@@ -92,41 +102,54 @@ def handle_events():
 
 player = None
 background =None
-mouse = None
+mouses = []
 mouse_num = 0
+num=0
 ui =None
 move = False
 running = True
 dir = 0
+x =0
 
 def draw_world():
     background.draw()
     ui.draw()
     player.draw()
-    mouse.draw()
+    for mouse in mouses:
+        mouse.draw()
 
 def enter():
-    global player, running, background, ui, mouse
+    global player, running, background, ui
     running = True
     ui= UI()
     player = Player()
     background =BG()
-    mouse = Mouse()
+    mouses.append(Mouse())
     pass
 
 def exit():
-    global player, background, mouse
-    del mouse
+    global player, background
+    for mouse in mouses:
+        del mouse
     del background
     del player
     pass
 
 def update():
     player.update()
-    mouse.update()
+    for mouse in mouses:
+        mouse.update()
 
 def draw():
     clear_canvas()
     draw_world()
     update_canvas()
 
+def pause():
+    pass
+
+def resume():
+    pass
+
+def add_mouse():
+    mouses.append(Mouse())
